2bd697eb30c70957fc220bbd45b89157d95c4d05
Fedyatch <fedosukgt@gmail.com>
2024-12-07 10:56:02 +0300

    Add files via upload

--- ex5.jl
+++ ex5.jl
@@ -1,79 +1,65 @@
+#=
+ДАНО: На ограниченном внешней прямоугольной рамкой поле имеется
+ровно одна внутренняя перегородка в форме прямоугольника. Робот - в
+произвольной клетке поля между внешней и внутренней перегородками.
+РЕЗУЛЬТАТ: Робот - в исходном положении и по всему периметру
+внутренней, как внутренней, так и внешней, перегородки поставлены маркеры
+=#
+
 using HorizonSideRobots
 
-
-function do_upora!(robot, side)
-    n = 0::Int
-    while !isborder(robot, side)
-        move!(robot, side)
-        n+=1
-    end
-    return n
+mutable struct MarkRobot
+    robot::Robot
 end
 
-function num_steps(robot, side)
-    n = 0
-    while !isborder(robot, side)
-        move!(robot, side)
-        n += 1
+function mark!(robot::MarkRobot)
+    corner!(robot, (Sud, West))
+    snake!(robot, (Ost, Nord)) do side
+        isborder(robot, side) && isborder(robot, Nord)
     end
-    do_upora!(robot, inverse(side))
-    return n
+    move!(robot, Sud)
+    snake!(robot, (West, Sud)) do side
+        isborder(robot, side) && isborder(robot, Sud)
+    end
 end
 
-inverse(side::HorizonSide) = HorizonSide((Int(side)+2)%4)
-
-function printer!(robot)
-    x = 0::Int
-    y = 0::Int
-    while !isborder(robot, Sud) || !isborder(robot, West)
-        x += do_upora!(robot, West)
-        y += do_upora!(robot, Sud)
+function HorizonSideRobots.move!(robot::MarkRobot, side::HorizonSide) 
+    if isborder(robot, Nord) || isborder(robot, Ost) || isborder(robot, Sud) || isborder(robot, West)
+        putmarker!(robot.robot)
     end
-    h = num_steps(robot, Nord)
-    side_1 = Ost
-    side_3 = Nord
-    for i in 1:h
-        while !isborder(robot, side_1)
-            for side_2 in (Sud, West, Nord, Ost)
-                if isborder(robot, side_2)
-                    putmarker!(robot)
-                end
-            end
-            move!(robot, side_1)
+    move!(robot.robot, side)
+end
+
+function snake!(stop_condition::Function, robot::MarkRobot, sides::NTuple{2, HorizonSide})
+    s = sides[1]
+    while !stop_condition(s)
+        movetoend!(robot, s) do
+            stop_condition(s) || isborder(robot, s)
         end
-        if isborder(robot, side_1)
+        if stop_condition(s)
             putmarker!(robot)
+            break
         end
-        if !isborder(robot, side_3)
-            move!(robot, side_3)
-        end
-        side_1 = inverse(side_1)
-    end
-    side_3 = Sud
-    for i in 1:h
-        while !isborder(robot, side_1)
-            for side_2 in (Sud, West, Nord, Ost)
-                if isborder(robot, side_2)
-                    putmarker!(robot)
-                end
-            end
-            move!(robot, side_1)
-        end
-        if isborder(robot, side_1)
-            putmarker!(robot)
-        end
-        if !isborder(robot, side_3)
-            move!(robot, side_3)
-        end
-        side_1 = inverse(side_1)
-    end
-    for i in 1:y
-        move!(robot, Nord)
-    end
-    for i in 1:x
-        move!(robot, Ost)
+        s = inverse(s)
+        move!(robot.robot, sides[2])
     end
 end
 
-inverse(side::HorizonSide) = HorizonSide((Int(side)+2)%4)
+movetoend!(stop_condition::Function, robot, side) = while !stop_condition() move!(robot, side) end
 
+function corner!(robot, sides::NTuple{2, HorizonSide})
+    while !isborder(robot, sides[1]) || !isborder(robot, sides[2])
+        movetoend!(robot, sides[1]) do
+            isborder(robot, sides[1])
+        end
+        movetoend!(robot, sides[2]) do
+            isborder(robot, sides[2])
+        end
+    end
+end
+
+HorizonSideRobots.isborder(robot::MarkRobot, side::NTuple{2, HorizonSide}) = isborder(robot, side[1]) || isborder(robot, side[2])
+HorizonSideRobots.isborder(robot::MarkRobot, side) = isborder(robot.robot, side)
+HorizonSideRobots.putmarker!(robot::MarkRobot) = putmarker!(robot.robot)
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2, 4))
+inverse(sides::NTuple{2, HorizonSide}) = inverse.(side)
