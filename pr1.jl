6f6a285660c0e334022b9ae62a38fedc2d11be1c
Fedyatch <fedosukgt@gmail.com>
2025-02-21 10:24:33 +0300

    Add files via upload

---
+++ pr1.jl
@@ -0,0 +1,29 @@
+iter = []
+function arraymaker(iter)
+    randn = rand(-10:10,10)
+    for i in randn
+        push!(iter, i)
+    end
+    println(iter)
+    return iter
+end
+
+
+
+function maxsum(iter)
+    arraymaker(iter)
+    mx = 0
+    sum = 0
+    for i in 1:length(iter)
+        if sum + iter[i] >= iter[i]
+            sum += iter[i]
+        else
+            sum = iter[i]
+            mx = max(sum, mx)
+        end
+        mx = max(sum, mx)
+    end
+    return mx
+end
+
+        
